2c5db08084879cc47106c087dfec14f7ea22cb46
Elsa Schieffelin <ems18@rice.edu>
2019-06-08 14:11:08 -0500

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,18 @@
 //set up SVG
-const SVGWIDTH = 960;
-const SVGHEIGHT = 500;
+const SVGWIDTH = 900;
+const SVGHEIGHT = 600;
 
 let svg=d3.select('.scatter')
 .append('svg')
-.attr('with', SVGWIDTH)
+.attr('width', SVGWIDTH)
 .attr('height', SVGHEIGHT);
 
 //set up margins
 const MARGIN = {
-    top: 10,
-    right: 10, 
-    bottom: 50,
-    left: 40
+    top: 100,
+    right: 50, 
+    bottom: 100,
+    left: 100
 };
 
 const SCATTERWIDTH = SVGWIDTH - MARGIN.left - MARGIN.right;
@@ -22,150 +22,92 @@ const SCATTERHEIGHT = SVGHEIGHT - MARGIN.top - MARGIN.bottom;
 const CHARTGROUP = svg.append('g')
     .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`);
 
-const CHOSENX = 'poverty';
+let chosenX = 'poverty';
+let chosenY = 'smokes';
 
 //create scalar functions
-function xScale(censusData,CHOSENX) {
-    var xLinearScale = d3.scaleLinear().domain([d3.min(censusData, d=>d[CHOSENX]) * 0.8,
-        d3.max(censusData, d=>d[CHOSENX]) * 2.1
-        ]).range([0, SCATTERWIDTH]);
+
+function xScale(censusData,chosenX) {
+    let xLinearScale = d3.scaleLinear()
+       .domain([d3.min(censusData, function(d) {return parseFloat(d[chosenX]) * 0.90}), d3.max(censusData, function(d) {return parseFloat(d[chosenX]) * 1.2})])
+       .range([0, SCATTERWIDTH]);
     return xLinearScale;
 }
 function yScale(censusData) {
-    var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(censusData, d=>d.smokes)])
+    let yLinearScale =d3.scaleLinear()
+    .domain([d3.min(censusData, function(d) {return parseFloat(d[chosenY]) *0.75}), d3.max(censusData, function(d) {return parseFloat(d[chosenY]) * 1.2})])
     .range([SCATTERHEIGHT, 0]);
     return yLinearScale;
 }
 
-function renderAxes(newXScale, xAxis) {
-    var bottomAxis = d3.axisBottom(newXScale);
-    xAxis.transition().duration(1000).call(bottomAxis);
-
-    return xAxis;
-}
-
-function renderCircles(circlesGroup, newXScale, CHOSENX) {
-    circlesGroup.transition()
-        .duration(1000)
-        .attr('cx', d => newXScale(d[CHOSENX]));
-    return circlesGroup;
-}
-function updateToolTip(CHOSENX, circlesGroup) {
-    if (CHOSENX === 'poverty') {
-        var label = 'Poverty:';
-    }
-    else {
-        var label = 'Smokers:'
-    }
-    var toolTip = d3.tip()
-        .attr('class', 'tooltip')
-        .offset([80,-60])
-        .html(function(x) {
-            return (`${x.state}<br>${label} ${x[CHOSENX]}`);
-        });
-    circlesGroup.call(toolTip);
-    circlesGroup.on('mouseover', function(data) {
-        toolTip.show(data);
-    });
-
-    return circlesGroup;
-}   
-
 // READ IN DATA //
 
-d3.csv("../assets/data/data.csv", function (censusData) {
-   // console.log(parseFloat(censusData.smokes));
-   
+d3.csv("../assets/data/data.csv").then(function (censusData) {
+    console.log(d3.max(censusData, d => d.poverty));
     //smokers vs. poverty
     //parse data
-        Object.entries(censusData).forEach(function(d) {
-       d.poverty = parseFloat(censusData.poverty);
-       d.smokes = parseFloat(censusData.smokes);
-       d.income = parseFloat(censusData.income);
-       });
+       censusData.forEach(function(d) {
+       d.poverty = parseFloat(d.poverty);
+       d.smokes = parseFloat(d.smokes);
+        });
+    
 
 //x and y scale
-    let xLinearScale = xScale(censusData, CHOSENX);
+    let xLinearScale = xScale(censusData, chosenX);
     let yLinearScale = yScale(censusData);
+
 //builds bottom and left axis
-    let bottomAxis = d3.axisBottom(xLinearScale);
-    let leftAxis = d3.axisLeft(yLinearScale);
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
 //adds axes to chartgroup
-    let xAxis = CHARTGROUP.append('g')
-        .classed('x-axis', true)
-        .attr('tranform', `translate(0, ${SCATTERHEIGHT})`)
+    CHARTGROUP.append('g')
+        .attr('transform', `translate(0, ${SCATTERHEIGHT})`)
         .call(bottomAxis);
     
     CHARTGROUP.append('g').call(leftAxis);
-//adds circles
+
+
+    //adds circles
     var circlesGroup = CHARTGROUP.selectAll('circle')
         .data(censusData)
         .enter()
         .append('circle')
-        .attr('cx', d=> xLinearScale(d[CHOSENX]))
-        .attr('cy', d=> yLinearScale(d.smokes))
-        .attr('r', 20)
-        .attr('fill', 'blue')
-        .attr('opacity', '.8');    
-//create 2 x axis labels
-    var labelsGroup = CHARTGROUP.append('g')
-        .attr('transform', `translate(${SCATTERWIDTH/2}, ${SCATTERHEIGHT+20})`);
-      
-    let povertyLabel = labelsGroup.append('text')
-        .attr('x', 0)
-        .attr('y', 20)
-        .attr('value', 'poverty')
-        .classed('active', true)
-        .text('Poverty (%)');
-    
+        .attr('cx', d=> xLinearScale(d[chosenX]))
+        .attr('cy', d=> yLinearScale(d[chosenY]))
+        .attr('r', '15')
+        .attr('fill', 'lightblue')
+        .attr('opacity', '1')
+        .attr('class', 'stateCircle')
+     
+    CHARTGROUP.selectAll('stateCircle')
+        .data(censusData)
+        .enter()
+        .append('text')
+        .attr('stateText', 'text')
+        .attr('x', d=>xLinearScale(d[chosenX]))
+        .attr('y', d=>yLinearScale(d[chosenY]))
+        .attr('dy', '0.3em')
+        .attr('dx', '-0.6em')
+        .text(d=>d.abbr);
 
-    //y label
-    var smokingLabel = labelsGroup.append('text')
-        .attr('x', 0)
-        .attr('y', 40)
-        .attr('value', 'smokes')
-        .classed('inactive', true)
-        .text('Smokers (%)');
- 
-    //append y 
+   
+        //.attr('text', `${censusData, d=> d.abbr}`);
+
+
+    
+    
+//labels
+
+    CHARTGROUP.append("text")
+    .attr("transform", `translate(${SCATTERWIDTH / 2}, ${SCATTERHEIGHT + MARGIN.top-50})`)
+    .attr("class", "x-axisText")
+    .text("State Population under Poverty Line (%)");
 
     CHARTGROUP.append('text')
         .attr('transform', 'rotate(-90)')
-        .attr('y', 0 - MARGIN.left)
+        .attr('y', 0 - MARGIN.left + 10)
         .attr('x', 0 - (SCATTERHEIGHT/2))
         .attr('dy', '1em')
-        .classed('axis-text', true)
-        .text('Average Household Income');
-    
-    var circlesGroup = updateToolTip(CHOSENX, circlesGroup);
-
-    labelsGroup.selectAll('text')
-        .on('click', function() {
-        var value = d3.select(this).att('value');
-        if (value !== CHOSENX) {
-            CHOSENX = value;
-            xLinearScale = xScale(censusData, CHOSENX);
-            xAxis = renderAxes(xLinearScape, xAxis);
-            circlesGroup = renderCircles(circlesGroup, xLinearScale, CHOSENX);
-            circlesGroup = updateToolTip(CHOSENX, circlesGroup);
-        
-        if (CHOSENX === 'smokes') {
-            smokingLabel
-                .classed('active', true)
-                .classed('inactive', false);
-            povertyLabel   
-                .classed('active', false)
-                .classed('inactive', true);
-        }
-        else {
-            smokingLabel
-                .classed('active', false)
-                .classed('inactive', true);
-            povertyLabel
-                .classed('active', true)
-                .classed('inactive', false);
-        }
-        }
-    });          
+        .attr('class', 'y-axisText')
+        .text('Smokers per State (%)');
     });
